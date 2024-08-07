fd53776875938885d44fdd1572bfe472337f3622
thiago quadros pedroso <thiago_q_pedroso@estudante.sesisenai.org.br>
2024-08-06 21:11:26 -0300

    kyubu-store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path:'',redirectTo:'catalogo',pathMatch:'full'},
+   {path:'catalogo',component:CatalogoComponent},
+   {path:'login',component:LoginComponent}
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
